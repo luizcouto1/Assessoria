b35f09636c0109b84a197411fe92afd5a7212661
hamilton <gabrielzinho.gabriel2@gmail.com>
2017-11-12 21:06:01 -0300

    Atualizando Javascript Candidatos, e implantando a tela listarcandidatos

--- src/main/java/com/htcursos/model/Candidato.java
+++ src/main/java/com/htcursos/model/Candidato.java
@@ -21,13 +21,25 @@ public class Candidato {
 	private String nome;
 	@NotEmpty
 	private String cargoPolitico;
-	@JoinColumn(name = "endereco_id")
-	@OneToOne
-	private Endereco endereco;
-	@JoinColumn(name = "contato_id")
-	@OneToOne
-	private Contato contato;
-	
+	@NotEmpty
+	private String email;
+	@NotEmpty
+	private String celular;
+	private String telefone;
+	@NotEmpty
+	private String cep;
+	@NotEmpty
+	private String cidade;
+	@NotEmpty
+	private String estado;
+	@NotEmpty
+	private String bairro;
+	@NotEmpty
+	private String rua;
+	@NotEmpty
+	private String numero;
+	@NotEmpty
+	private String complemento;
 	public long getId() {
 		return id;
 	}
@@ -52,38 +64,87 @@ public class Candidato {
 	public void setCargoPolitico(String cargoPolitico) {
 		this.cargoPolitico = cargoPolitico;
 	}
-	public Endereco getEndereco() {
-		return endereco;
+
+	public String getEmail() {
+		return email;
 	}
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-	public Contato getContato() {
-		return contato;
+	public String getCelular() {
+		return celular;
 	}
-	public void setContato(Contato contato) {
-		this.contato = contato;
+	public void setCelular(String celular) {
+		this.celular = celular;
 	}
-	
-	@Override
-	public String toString() {
-		return "Candidato [id=" + id + ", partido=" + partido + ", nome=" + nome + ", cargoPolitico=" + cargoPolitico
-				+ ", endereco=" + endereco + ", funcionario=" + ", contato=" + contato + "]";
+	public String getTelefone() {
+		return telefone;
 	}
-	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((bairro == null) ? 0 : bairro.hashCode());
 		result = prime * result + ((cargoPolitico == null) ? 0 : cargoPolitico.hashCode());
-		result = prime * result + ((contato == null) ? 0 : contato.hashCode());
-		result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+		result = prime * result + ((celular == null) ? 0 : celular.hashCode());
+		result = prime * result + ((cep == null) ? 0 : cep.hashCode());
+		result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
+		result = prime * result + ((complemento == null) ? 0 : complemento.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((estado == null) ? 0 : estado.hashCode());
 		result = prime * result + (int) (id ^ (id >>> 32));
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
 		result = prime * result + ((partido == null) ? 0 : partido.hashCode());
+		result = prime * result + ((rua == null) ? 0 : rua.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
 		return result;
 	}
-	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -93,20 +154,45 @@ public class Candidato {
 		if (getClass() != obj.getClass())
 			return false;
 		Candidato other = (Candidato) obj;
+		if (bairro == null) {
+			if (other.bairro != null)
+				return false;
+		} else if (!bairro.equals(other.bairro))
+			return false;
 		if (cargoPolitico == null) {
 			if (other.cargoPolitico != null)
 				return false;
 		} else if (!cargoPolitico.equals(other.cargoPolitico))
 			return false;
-		if (contato == null) {
-			if (other.contato != null)
+		if (celular == null) {
+			if (other.celular != null)
 				return false;
-		} else if (!contato.equals(other.contato))
+		} else if (!celular.equals(other.celular))
 			return false;
-		if (endereco == null) {
-			if (other.endereco != null)
+		if (cep == null) {
+			if (other.cep != null)
 				return false;
-		} else if (!endereco.equals(other.endereco))
+		} else if (!cep.equals(other.cep))
+			return false;
+		if (cidade == null) {
+			if (other.cidade != null)
+				return false;
+		} else if (!cidade.equals(other.cidade))
+			return false;
+		if (complemento == null) {
+			if (other.complemento != null)
+				return false;
+		} else if (!complemento.equals(other.complemento))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (estado == null) {
+			if (other.estado != null)
+				return false;
+		} else if (!estado.equals(other.estado))
 			return false;
 		if (id != other.id)
 			return false;
@@ -115,13 +201,36 @@ public class Candidato {
 				return false;
 		} else if (!nome.equals(other.nome))
 			return false;
+		if (numero == null) {
+			if (other.numero != null)
+				return false;
+		} else if (!numero.equals(other.numero))
+			return false;
 		if (partido == null) {
 			if (other.partido != null)
 				return false;
 		} else if (!partido.equals(other.partido))
 			return false;
+		if (rua == null) {
+			if (other.rua != null)
+				return false;
+		} else if (!rua.equals(other.rua))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
 		return true;
 	}
+	@Override
+	public String toString() {
+		return "Candidato [id=" + id + ", partido=" + partido + ", nome=" + nome + ", cargoPolitico=" + cargoPolitico
+				+ ", endereco=" +  ", email=" + email + ", celular=" + celular + ", telefone=" + telefone
+				+ ", cep=" + cep + ", cidade=" + cidade + ", estado=" + estado + ", bairro=" + bairro + ", rua=" + rua
+				+ ", numero=" + numero + ", complemento=" + complemento + "]";
+	}
+	
 	
 	
 }
