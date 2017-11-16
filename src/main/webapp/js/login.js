1eba07ddd755c2248fb3b8353f651bfc970e6879
luizcouto3 <luizcouto3@gmail.com>
2017-11-16 14:38:19 -0200

    tela cadastro usuario

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -8,18 +8,17 @@ $(document).ready(function(){
 		  });
 		});
 
-		$(".close-btn").click(function(){
+	$(".close-btn").click(function(){
 		  TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
 		  TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
 		  $("#container, #forgotten-container").fadeOut(800, function(){
 		    $("#login-button").fadeIn(800);
 		  });
 		});
-
+	
 		$('#forgotten').click(function(){
 		  $("#container").fadeOut(function(){
 		    $("#forgotten-container").fadeIn();
 		  });
 		});
 });
-
---
+++ src/main/webapp/js/telaprincipal.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$('.dropdown-content').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, 
+      hover: true, 
+      gutter: 0, 
+      belowOrigin: false, 
+      alignment: 'left',
+      stopPropagation: false 
+    }
+  );
+});
